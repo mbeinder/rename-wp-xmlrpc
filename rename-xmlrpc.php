fbb26bcfc0b7eb5804fa34ae4a18aa44fa79ec4b
Marc Beinder <marc@encoremediasystems.com>
2020-08-21 09:36:27 -0500

    Installed New Software Updater

--- rename-xmlrpc.php
+++ rename-xmlrpc.php
@@ -4,15 +4,16 @@ Plugin Name: XMLRPC Mask
 Plugin URI: https://onairmarc.com
 Description: Customized version of Rename XMLRPC from Jorge Bernal. Make XML-RPC work if you rename the file. Some hosts block access to xmlrpc.php file making it impossible to use
 Author: Marc Beinder
-Version: 1.0
+Version: 1.1
 Author URI: https://onairmarc.com
 */
 
-require 'plugin-update-checker/plugin-update-checker.php';
+// require 'plugin-update-checker/plugin-update-checker.php';
+require 'plugin-update-checker-20200821_update/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 	'https://github.com/mbeinder/rename-wp-xmlrpc/',
 	__FILE__,
-	'unique-plugin-or-theme-slug'
+	'rename-wp-xmlrpc'
 );
 
 //Optional: If you're using a private repository, specify the access token like this:
